b1bb0e3e9d61ec3a20502f9350b644c54cf0dac1
Girish Kumar <57340654+gk2savage@users.noreply.github.com>
2021-07-12 20:18:36 +0530

    Update s3downloader.py

--- s3downloader.py
+++ s3downloader.py
@@ -4,9 +4,9 @@ root_dir = CloudPath("s3://bucketname")
 
 path= 'C:/Users/g2/Desktop'
 
-for f in root_dir.glob('**/*.txt'):
+for f in root_dir.glob('**/*.json'):
     text_data = f.read_text()
     print(f)            #Print all files and directories in bucket
-    print(text_data)    #Print all content from .txt
+    print(text_data)    #Print all content from all the .json files inside the bucket
     
 root_dir.download_to(path)  #Download whole bucket to defined path
